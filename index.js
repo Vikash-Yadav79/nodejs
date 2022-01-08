b0b8836702df5677cf531ea1fd8b4f53e5dbc6e7
2017-v <75252470+2017-v@users.noreply.github.com>
2022-01-08 08:44:49 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+const fs = require("fs");
+// fs.mkdirSync("Vikash");
+
+
+// fs.writeFileSync('Vikash/bio.txt', "My Name is vikash yadav");
+
+// fs.appendFileSync('vikash/bio.txt',  "this is nodejs assessment");
+
+// const data = fs.readFileSync("Vikash/bio.txt", "utf8");
+// console.log(data);
+
+// fs.renameSync("Vikash/bio.txt" , "vikash/myBio.txt");
+
+fs.rmdirSync("vikash");
